19b2735a0c48346c2b8a204e77347e280a474e7f
rahiblaghari <rlaghari@gmail.com>
2022-10-25 15:49:07 -0500

    makes file uploaded into a text file as well

--- server.js
+++ server.js
@@ -15,16 +15,31 @@ http.createServer(function (req, res) {
   
             var tempFilePath = files.filetoupload.filepath;
             var projectFilePath = __dirname + '/uploaded_file/' + "targetFile.pdf" //files.filetoupload.originalFilename;
+            let textversion = __dirname + '/uploaded_file/' + "textverson.txt" 
                 
                 fs.rename(tempFilePath, projectFilePath, function (err) {
                 if (err) throw err;
                 res.write('File has been successfully uploaded' + projectFilePath);
                 res.end();
 
+                let holder = "";
+
                 new PdfReader().parseFileItems(projectFilePath, (err, item) => {
                     if (err) console.error("error:", err);
-                    else if (!item) console.warn("end of file");
-                    else if (item.text) console.log(item.text);
+                    else if (!item)
+                    { 
+                        console.log(holder);
+                        // fs.writeFile('/uploaded_file/textversion.txt', holder)
+                        fs.writeFile('./uploaded_file/textversion.txt', holder, function (err) {
+                            if (err) throw err;
+                            console.log('File is created successfully.');
+                          });
+                        console.warn("end of file");
+                    }
+                    else if (item.text){
+                        holder += item.text + " ";
+                    }
+                    // { console.log(item.text);}
                   });
             });
         });
